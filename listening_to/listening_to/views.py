52b0e8e2b2b23403ec5ca7b8417ab7b2ccd7fdeb
Newname <therevisto@gmail.com>
2023-08-23 01:14:37 +0430

    fix index names

--- listening_to/listening_to/views.py
+++ listening_to/listening_to/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def members(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
