0ecf6824907707207fb6569d6d66d5b428bef4f5
revisto <therevisto@gmail.com>
2023-08-25 14:17:06 +0430

    create spotify object in the first

--- listening_to/listening_to/api/views.py
+++ listening_to/listening_to/api/views.py
@@ -2,6 +2,8 @@ from listening_to.api.spotify import Spotify
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+spotify = Spotify()
+
 @api_view(['GET'])
 def currently_playing_song(request):
-    return Response(Spotify().currently_playing())
+    return Response(spotify.currently_playing())
